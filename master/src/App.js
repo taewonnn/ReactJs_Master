a7640fc3d3533226d0c2706eba825fc7477106b1
TaeWon Park <thisis_tw94@naver.com>
2023-03-20 23:09:01 +0900

    #2.7 theme

--- master/src/App.js
+++ master/src/App.js
@@ -1,107 +1,27 @@
 import styled, {keyframes} from "styled-components";
 
 
-// styledComponent의 편의성
-// className을 알아서 지정해주고 내 코드를 저장해둠!
-// + 중복하지 않을 수 있도록 도와줘!!
-
-
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
-`
-
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`
-
-const Circle = styled(Box)`
-  border-radius: 50px;
-`
-
-// animation  -> import {keyframes}
-
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0;
-  }
-  50% {
-    transform: rotate(360deg);
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(0deg);
-    border-radius: 50px;
-  }
-`
-
-
-const Emoji = styled.span`
-  font-size: 36px;
-`
-
-const AnimationBox = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
+  height: 100vh;
+  width: 100vw;
   justify-content: center;
   align-items: center;
-  animation: ${rotationAnimation} 1s linear infinite;
-  
-  // span은 styledComponent가 아니지만, AnimationBox안에 있으므로 지정해줄 수 있음!!
-  
-  // ${Emoji}로 하면 emoji가 div이던 p이던 span이던 상관없이 지정이 가능함!!
-  ${Emoji} {
-    font-size: 100px;
-    
-    &:hover {
-      font-size: 200px;
-    }
-    &:active {
-      opacity: 0;
-    }
-  }
+  background-color: ${props => props.theme.backgroundColor}};
   
 `
 
-
-// as 개념 => Btn의 속성은 그대로 가져가고 싶은데 button이 아니라 a태그로 가져가고 싶을 때,
-// <Btn as="a"> 해주면 속성은 Btn의 것을 가져오지만, a태그의 속성임!!!
-const Btn = styled.button`
-  color: white;
-  background-color: tomato;
-  border: 0;
-  border-radius: 15px;
-`
-
-
-// attrs({})
-// {} 안에 속성들을 미리 넣어두고 컴포넌트는 그냥 <Input>만 사용가능!!
-const Input = styled.input.attrs({required: true, minLength: 10})`
-  background-color: tomato;
+const Title = styled.h1`
+  color : ${props => props.theme.textColor}
 `
 
 function App() {
 
   return (
 
-    <Father as="header">
-      {/*<Box bgColor = "teal"/>*/}
-      {/*<Box bgColor = "tomato"/>*/}
-      {/*<Circle bgColor = "red"/>*/}
-      {/*<Btn>Login</Btn>*/}
-      {/*<Btn as="a" href = '/'>Login</Btn>*/}
-      {/*<br/>*/}
-      {/*<Input />*/}
-      {/*<Input />*/}
-      <AnimationBox>
-        <Emoji as = 'p'>🐿</Emoji>
-      </AnimationBox>
-
-    </Father>
+    <Wrapper>
+      <Title>daram</Title>
+    </Wrapper>
 
   );
 }
--- master/src/index.js
+++ master/src/index.js
@@ -1,8 +1,24 @@
 import React from "react";
 // import ReactDOM from 'react-dom'; //구버전
 import { createRoot } from "react-dom/client";
+import {ThemeProvider} from "styled-components";
 import App from "./App";
+
+
+const darkTheme = {
+  textColor: 'whitesmoke',
+  backgroundColor: 'black',
+}
+
+const lightTheme = {
+  textColor: 'black',
+  backgroundColor: 'whitesmoke'
+}
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={darkTheme}>
+    <App />
+  </ThemeProvider>
+);
